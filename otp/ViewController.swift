151e113e401b0ef941920304f200c5b9a912d0e5
Andy Marthin <mr.andymarthin@gmail.com>
2018-03-09 00:11:48 +0700

    initial commit

--- otp/ViewController.swift
+++ otp/ViewController.swift
@@ -10,9 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textOne: UITextField!
+    @IBOutlet weak var textTwo: UITextField!
+    @IBOutlet weak var textFour: UITextField!
+    @IBOutlet weak var textThree: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    
+        textOne.keyboardType = UIKeyboardType.numberPad
+        textTwo.keyboardType = UIKeyboardType.numberPad
+        textThree.keyboardType = UIKeyboardType.numberPad
+        textFour.keyboardType = UIKeyboardType.numberPad
+        
+        textOne.addTarget(self, action: #selector(self.textFieldDidChange(textField:)), for: UIControlEvents.editingChanged)
+        textTwo.addTarget(self, action: #selector(self.textFieldDidChange(textField:)), for: UIControlEvents.editingChanged)
+        textThree.addTarget(self, action: #selector(self.textFieldDidChange(textField:)), for: UIControlEvents.editingChanged)
+        textFour.addTarget(self, action: #selector(self.textFieldDidChange(textField:)), for: UIControlEvents.editingChanged)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        textOne.becomeFirstResponder()
+    }
+    
+    
+    @objc func textFieldDidChange(textField: UITextField){
+        let text = textField.text
+        
+        if text?.utf8.count == 1 {
+            switch textField{
+            case textOne:
+                textTwo.becomeFirstResponder()
+            case textTwo:
+                textThree.becomeFirstResponder()
+            case textThree:
+                textFour.becomeFirstResponder()
+            case textFour:
+                textFour.resignFirstResponder()
+            default:
+                break
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
